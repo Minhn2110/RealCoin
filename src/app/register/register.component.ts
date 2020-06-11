b322d32e296f9d113b53a6da8e6714da50a69c11
Minh <minhnha@smartosc.com>
2020-06-11 15:19:40 +0700

    uppp

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthenticationService } from '../_services/authentication.service';
 import { first } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -40,12 +41,45 @@ export class RegisterComponent implements OnInit {
       data => {
         console.log(data);
         // this.router.navigate([this.returnUrl]);
-        this.router.navigate(['/login']);
+        this.showMsg('Register', 'Register Success')
+        setTimeout(() => {
+          this.router.navigate(['/login']);
+        }, 2100);
       },
       error => {
         this.error = error.Message;
         console.log(error);
       });
+  }
+
+  showMsg(title, msg): void {
+    let timerInterval
+    swal.fire({
+      title: title,
+      html: msg,
+      timer: 2000,
+      timerProgressBar: true,
+      onBeforeOpen: () => {
+        swal.showLoading()
+        timerInterval = setInterval(() => {
+          const content = swal.getContent();
+          if (content) {
+            const b = content.querySelector('b');
+            if (b) {
+              b.textContent = swal.getTimerLeft().toString();
+            }
+          }
+        }, 100)
+      },
+      onClose: () => {
+        clearInterval(timerInterval)
+      }
+    }).then((result) => {
+      /* Read more about handling dismissals below */
+      if (result.dismiss === swal.DismissReason.timer) {
+        console.log('I was closed by the timer')
+      }
+    })
   }
 
 }
