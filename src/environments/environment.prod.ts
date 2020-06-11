89183464de905fcb51fd753d80e8c6736b90c30b
Minh <minhnha@smartosc.com>
2020-06-11 15:52:52 +0700

    up firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,3 +3,14 @@ export const environment = {
   production: true,
   apiUrl: 'http://localhost:4200'
 };
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCsFwZxACzW_uOhMbBtYRG2N3BfWQ0eZjI",
+  authDomain: "coin-market-2020.firebaseapp.com",
+  databaseURL: "https://coin-market-2020.firebaseio.com",
+  projectId: "coin-market-2020",
+  storageBucket: "coin-market-2020.appspot.com",
+  messagingSenderId: "311113033301",
+  appId: "1:311113033301:web:23bf999f7684dcb2ccd6dc",
+  measurementId: "G-ZKYL9Q9W6R"
+};
